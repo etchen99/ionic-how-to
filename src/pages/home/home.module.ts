18aaac2b3b21b21b735a48f25ed500659087371e
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-01-27 11:23:44 -0200

    Add header in a shared module

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
+import { SharedModule } from '../../shared/shared.module';
 import { HomePage } from './home';
 
 @NgModule({
   declarations: [HomePage],
   exports: [HomePage],
-  imports: [IonicPageModule.forChild(HomePage)],
+  imports: [IonicPageModule.forChild(HomePage), SharedModule],
 })
 export class HomePageModule {}
